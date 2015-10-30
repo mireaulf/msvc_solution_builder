e1d4b804bed967793655e65db1630d80011cee8d
soda <mireaulf+github@gmail.com>
2015-10-29 21:10:31 -0400

    Add prop sheets

---
+++ src/NewProject/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include <Windows.h>
+
+int main(int argc, char** argv) {
+    UNREFERENCED_PARAMETER(argc);
+    UNREFERENCED_PARAMETER(argv);
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
